2185632955a62a036ed653f02297f83d051b3409
wslYEEEE <darrenthoo@gmail.com>
2026-04-30 17:55:18 +0800

    start wrangling path

--- req/string.c
+++ req/string.c
@@ -1,5 +1,34 @@
 #include "h_minishell.h"
 
+char	*longest_argv(env, char *src)
+{
+	t_shnode	*iter;
+	char		*ret;
+	int			max;
+	int			len;
+
+	max = 0;
+	iter = env->env;//ofc its wring you have to split path first
+	while (iter)
+	{
+		len = 0;
+		if (iter->str)
+			len = ft_strlen(iter->str);
+		if (len > max)
+			max = len;
+		iter = iter->next;
+	}
+	ret = NULL;
+	if (max)
+	{
+		ret = malloc(max + ft_strlen(src) + 1);
+		ft_err(-!ret, "cmd malloc error");
+	}
+//	if (ret)
+//		ret[0] = '\0';
+	return (ret);
+}
+
 //recontruct
 //will ironically be called from redir_to_fd
 char    **make_argv(t_tok *src, t_env *env)
